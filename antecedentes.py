c7e6dfed75f0134dbd351328e690aa77d4b1ff0b
Maruja <maria.eugenia.lopez.ramirez11@gmail.com>
2025-06-17 14:12:41 -0700

    feature: antecedentes basemodel

---
+++ antecedentes.py
@@ -0,0 +1,18 @@
+
+from pydantic import BaseModel,Field
+from typing import Annotated,Literal
+from datetime import date
+
+class Antecedentes(BaseModel):
+    solicitante : Annotated[str, Field(max_length=100), "Name of the person that requires the avaluo."]
+    perito_valuador : Annotated[str, Field(max_length=100),"Name of the professional perito"]
+    asesor_valuador : Annotated[str, Field(max_length=100), "Name of the not professional asesor"]
+    fecha_del_avaluo : Annotated[date, "The date when the avaluo is made"]
+    tipo_inmueble_a_valuar : Literal["CASA_HABITACIÓN",	"CASA_HABITACIÓN_CON_COMERCIO_EN_PLANTA_BAJA", "DEPARTAMENTO", "BODEGA", "LOCAL_COMERCIAL", "INDUSTRIA", "NAVE_Y_OFICINAS", "EDIFICIO_DE_PRODUCTOS"]
+    regimen_de_propiedad : Literal["PRIVADA_INDIVIDUAL", "EN_CONDOMINIO", "COPROPIEDAD"]
+    propietario_del_inmueble : Annotated[str, Field(max_length=100), "person that owns the property and who is in the escrituras"]
+    proposito_de_avaluo : Literal["ESTIMAR_SU_VALOR_COMERCIAL", "DETERMINAR_SU_VALOR_COMERCIAL"]
+    ubicacion_del_inmueble : Annotated[str, Field(max_length=1000) ,"address where the residence is located"]
+    numero_de_cuenta_predial : Annotated[str, Field(max_length=19) ,"this cannot be null value either it does not have or it hasnt been given"]
+    numero_de_cuenta_de_agua : Annotated[str, Field(max_length=19) ,"this cannot be null value either it does not have or it hasnt been given"]
+
--- main.py
+++ main.py
@@ -1,37 +1,11 @@
-#No me borres guey :V
-# from choya.db.model import SessionLocal,  AvaluoCaractetistica, TipoCaracteristicaAvaluo
-# from functools import reduce
-#from typing import Annotated
-#Char el  second approach  no me borres wey
-#from choya.config.avaluo_configuration import load_app_properties, load_yaml_second_approach
 
 from fastapi import FastAPI
 from choya.router.input_router import router_input
+from antecedentes import Antecedentes
 
 app = FastAPI()
+
 app.include_router(router=router_input)
-
-#TODO TBD
-def avaluo_settings_loader():
-    pass
-    #No me borres guey :V
-    # db = SessionLocal()
-    # zona : AvaluoCaractetistica = AvaluoCaractetistica(titulo = "Alfa",descripcion = "asas",tipo = "OTROS_SERVICIOS", ponderacion  = 0.0)
-    # db.add(zona)
-    # db.commit()
-    # db.refresh(zona)
-    
-    # zonas = db.query(AvaluoCaractetistica).all()
-    # print(zonas)
-    
-    #Firs approach
-    # properties = load_app_properties()
-    # for avaluo in properties.AVALUO_CARACTERSITICAS_LIST:
-    #     print(avaluo)
-
-    # caracteristicas = load_yaml_second_approach()
-    # for c in caracteristicas.AVALUO_CARACTERSITICAS_LIST:
-    #     print(c)
-    #     print("*"*8)
-    
-
+@app.post("/antecedentes")
+def antecedentes(antecedente : Antecedentes):
+    return True
