39a846e0db712eadc82caaca9eafd6927baca154
Maruja <maria.eugenia.lopez.ramirez11@gmail.com>
2025-06-18 19:13:57 -0700

    feature: refactor of antecedentes Base Model.

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 
 from fastapi import FastAPI
 from choya.router.input_router import router_input
-from antecedentes import Antecedentes
+from choya.model.antecedentes import Antecedentes
 
 app = FastAPI()
 
 app.include_router(router=router_input)
-@app.post("/antecedentes")
-def antecedentes(antecedente : Antecedentes):
-    return True
+
